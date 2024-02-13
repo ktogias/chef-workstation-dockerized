49531f9d4921d790d006ce2ed784c607d2137d46
Konstantinos Togias <ktogias@gmail.com>
2024-02-13 18:08:53 +0200

    pull prebuild image and shell fixes

--- dchef.sh
+++ dchef.sh
@@ -7,17 +7,16 @@ test -f ~/.gitconfig || touch ~/.gitconfig
 
 docker run \
     --rm \
-    --user $UID:`id -g` \
+    --user $UID:"$(id -g)" \
     --workdir="/home" \
     --network host \
     -it \
     -v .:/home \
     -v /etc/group:/etc/group:ro \
     -v /etc/passwd:/etc/passwd:ro \
-    -v /etc/shadow:/etc/shadow:ro \
     -v ~/.gitconfig:/home/.gitconfig:ro \
     -v ~/.chef:/.chef \
     -v ~/.kitchen:/.kitchen \
     -v ~/.vagrant.d:/.vagrant.d \
     -v /var/run/libvirt/libvirt-sock:/var/run/libvirt/libvirt-sock \
-    dchef $@
+    ktogias/dchef "$@"
